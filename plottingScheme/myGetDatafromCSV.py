fe8fc5a53077d596b4ed2128d68cebf49ab093d7
Hugo Hiroshi Yamamura <hugoh@cpqd.com.br>
2016-03-14 16:46:26 -0300

    Removes tested files

--- plottingScheme/myGetDatafromCSV.py
+++
@@ -1,9 +0,0 @@
-import csv
-import sys
-
-delimiter_char = ';';
-
-with open(sys.argv[1] , 'rb') as csvfile:
-    reader = csv.reader(csvfile, delimiter=delimiter_char)
-    for column in reader:
-        print column
